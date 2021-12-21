6dfdc84a3879fb483dada48a09c835ece4914696
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2021-12-21 15:34:21 +0400

    Add 'notes' and 'optional' folders

---
+++ day_01/optional/ThreeSort.java
@@ -0,0 +1,3 @@
+public class ThreeSort{
+	// Write a program that takes three integer command-line arguments and prints them in ascending order. Use Math.min() and Math.max().
+}
---
+++ day_01/optional/UniformRandomNumbers.java
@@ -0,0 +1,3 @@
+public class UniformRandomNumbers{
+	//Write a program that prints five uniform random numbers between 0 and 1, their average value, and their minimum and maximum values. Use Math.random(), Math.min(), and Math.max().
+}
