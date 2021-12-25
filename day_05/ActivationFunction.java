441880f623368e2341c46ed40518f52e2dba4c6a
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2021-12-25 14:32:57 +0400

    Add ActivationFunction  solution

---
+++ day_05/ActivationFunction.java
@@ -0,0 +1,62 @@
+public class ActivationFunction {
+
+  public static double heaviside(double x) {
+    if (Double.isNaN(x)) {
+      return Double.NaN;
+    }
+    if (x < 0) {
+      return 0;
+    }
+    if (x == 0) {
+      return 0.5;
+    }
+    return 1.0;
+  }
+
+  public static double sigmoid(double x) {
+    if (Double.isNaN(x)) {
+      return Double.NaN;
+    }
+    return 1 / (1 + Math.pow(Math.E, -x));
+  }
+
+  public static double tanh(double x) {
+    if (Double.isNaN(x)) {
+      return Double.NaN;
+    }
+    return (Math.exp(x) - Math.exp(-x)) / (Math.exp(x) + Math.exp(-x));
+  }
+
+  public static double softsign(double x) {
+    if (Double.isNaN(x)) {
+      return Double.NaN;
+    }
+    return x / (1 + Math.abs(x));
+  }
+
+  public static double sqnl(double x) {
+    if (Double.isNaN(x)) {
+      return Double.NaN;
+    }
+    if (x <= -2) {
+      return -1.0;
+    }
+    if (x < 0) {
+      return (x + (x * x) / 4);
+    }
+    if (x < 2) {
+      return (x - (x * x) / 4);
+    }
+    return 1.0;
+  }
+
+  public static void main(String[] args) {
+    double x = Double.parseDouble(args[0]);
+
+    System.out.println("heaviside(" + x + ") = " + heaviside(x));
+    System.out.println("sigmoid(" + x + ") = " + sigmoid(x));
+    System.out.println("tanh(" + x + ") = " + tanh(x));
+    System.out.println("softsign(" + x + ") = " + softsign(x));
+    System.out.println("sqnl(" + x + ") = " + sqnl((x)));
+  }
+}
