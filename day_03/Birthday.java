9c7292c27bb315436b6cac1409b005cb1c035584
Mushegh Yeghiazaryan <mush.yegh@gmail.com>
2022-01-25 15:13:18 +0400

    Add Birthday (not completed)

---
+++ day_03/Birthday.java
@@ -0,0 +1,8 @@
+public class Birthday {
+  public static void main(String[] args) {
+    final int n = Integer.parseInt(args[0]);
+    final int trials = Integer.parseInt(args[1]);
+
+    // TO DO
+  }
+}
